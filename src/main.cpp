8eae0687fd47c2e204672d2ebd294fc52ed937d0
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-03-20 19:16:35 +0000

    Changed working of SensorData class

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,14 @@
 
 #include "Sensors/SensorData.hpp"
 
-SensorData sensorData_;
-
 void setup()
 {
     Serial.begin(115200);
+    Sensors.Begin();
+
+#ifndef ESP8266
+    while (!Serial); // wait for serial port to connect. Needed for native USB
+#endif
 
     // Init SD Card on correct SPI port.
     SDCard::Begin();
@@ -23,10 +26,50 @@ void setup()
 
 void loop()
 {
-    dlogn("Sats: %d", sensorData_.Satellites());
+    dlogn("Sats: %d", Sensors.Satellites());
 
-    dlogn("RTC Time: %s.", sensorData_.Time().timestamp().c_str());
+    byte error, address;
+    int nDevices;
+    dlogn("Scanning...");
+    nDevices = 0;
+    for (address = 1; address < 127; address++)
+    {
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+        if (error == 0)
+        {
+            dlog("I2C device found at address 0x");
+            if (address < 16)
+            {
+                dlogs("0");
+            }
+            dlogsn("%x", address);
+            nDevices++;
+        } else if (error == 4)
+        {
+            dlog("Unknow error at address 0x");
+            if (address < 16)
+            {
+                dlogs("0");
+            }
+            dlogsn("%x", address);
+        }
+    }
+    if (nDevices == 0)
+    {
+        dlogn("No I2C devices found");
+    } else
+    {
+        dlogn("done");
+    }
 
-    sensorData_.SmartDelay(1000);
+    dlogn("%s", Sensors.Timestamp());
+
+
+    dlogn("Temperature: %.2f *C", Sensors.Temperature());
+    dlogn("Pressure: %0.2f hPa", Sensors.Pressure() / 100);
+    dlogn("Approx Altitude: %0.2f m", Sensors.Altitude());
+
+    Sensors.SmartDelay(1000);
 
 }
