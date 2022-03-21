d05b6400f23ee61b6b7dc2bf3b654740dff5b72b
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-03-21 18:06:16 +0000

    Changed SD Pin configuration

--- src/PinConfiguration.hpp
+++ src/PinConfiguration.hpp
@@ -14,10 +14,10 @@
 #define LoRa_DIO0 26 /**< @brief Used by LoRa module */
 #define LoRa_RST 14 /**< @brief Used by LoRa module */
 
-#define SD_SCK 12 /**< @brief SD clock pin */
-#define SD_CS 13 /**< @brief SD chip select pin */
-#define SD_MISO 32 /**< @brief SD miso pin */
-#define SD_MOSI 33 /**< @brief SD mosi pin */
+#define SD_SCK 13 /**< @brief SD clock pin */
+#define SD_CS 27 /**< @brief SD chip select pin */
+#define SD_MISO 12 /**< @brief SD miso pin */
+#define SD_MOSI 14 /**< @brief SD mosi pin */
 
 #define GPS_RX 32 /**< @brief GPS rx pin */
 #define GPS_TX 33 /**<@brief GPS tx pin */
