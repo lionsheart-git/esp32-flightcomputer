e44a0a66226c6130fc1fc7b2baf3d0e26fc3805f
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-06-12 14:19:58 +0200

    Implemented simple apogee detection

---
+++ src/FlightControl/FlightControl.cpp
@@ -0,0 +1,41 @@
+
+#include "FlightControl.hpp"
+
+#include "Logging/SystemLogger.hpp"
+
+#define ALTITUDE_THRESHOLD 2
+
+FlightControl::FlightControl(SensorData &sensors)
+: sensors_(sensors), state_(FlightState::Idle), maxAltitude_(0.0), counter_(0)
+{
+    slog_i("Started flight control.");
+}
+
+FlightState FlightControl::State()
+{
+    return state_;
+}
+
+void FlightControl::checkFlight()
+{
+    if (isApogee()) {
+        slog_i("Eject Recovery");
+        state_ = FlightState::Descend;
+    }
+}
+
+bool FlightControl::isApogee()
+{
+    double currentAltitude = sensors_.AltitudeAboveGround();
+
+    if (maxAltitude_ < currentAltitude + ALTITUDE_THRESHOLD) {
+        maxAltitude_ = currentAltitude;
+        counter_ = 0;
+    } else if (currentAltitude + ALTITUDE_THRESHOLD < maxAltitude_ && counter_ > 100) {
+        slog_i("Apogee detected!");
+        return true;
+    } else if (currentAltitude + ALTITUDE_THRESHOLD < maxAltitude_) {
+        counter_++;
+    }
+    return false;
+}
---
+++ src/FlightControl/FlightControl.hpp
@@ -0,0 +1,36 @@
+
+#ifndef ESP32_FLIGHTCOMPUTER_FLIGHTCONTROLL_HPP
+#define ESP32_FLIGHTCOMPUTER_FLIGHTCONTROLL_HPP
+
+#include <Sensors/SensorData.hpp>
+
+enum FlightState {
+    Idle,
+    LaunchDetection,
+    PoweredFlight,
+    UnpoweredFlight,
+    Descend,
+    TouchedDown
+};
+
+
+class FlightControl
+{
+public:
+    explicit FlightControl(SensorData &sensors);
+    FlightState State();
+    void checkFlight();
+
+
+private:
+    bool isApogee();
+
+    SensorData& sensors_;
+    FlightState state_;
+
+    double maxAltitude_;
+    uint8_t counter_;
+};
+
+
+#endif //ESP32_FLIGHTCOMPUTER_FLIGHTCONTROLL_HPP
