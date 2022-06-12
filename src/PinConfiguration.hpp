dbc95e5fed87212186e08fd3c20b046d879b3174
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-06-12 14:20:34 +0200

    Restructured pin names

--- src/PinConfiguration.hpp
+++ src/PinConfiguration.hpp
@@ -7,17 +7,21 @@
 #ifndef FLIGHTCOMPUTER_PINCONFIGURATION_HPP
 #define FLIGHTCOMPUTER_PINCONFIGURATION_HPP
 
-#define LoRa_SCK 5 /**< @brief Used by LoRa module */
-#define LoRa_CS 18 /**< @brief Used by LoRa module */
-#define LoRa_MISO 19 /**< @brief Used by LoRa module */
-#define LoRa_MOSI 27 /**< @brief Used by LoRa module */
-#define LoRa_DIO0 26 /**< @brief Used by LoRa module */
-#define LoRa_RST 14 /**< @brief Used by LoRa module */
+#define SPI1_SCK 13
+#define  SPI1_MISO 12
+#define  SPI1_MOSI 14
 
-#define SD_SCK 13 /**< @brief SD clock pin */
+#define LoRa_SCK  SPI1_SCK /**< @brief Used by LoRa module */
+#define LoRa_CS 19 /**< @brief Used by LoRa module */
+#define LoRa_MISO SPI1_MISO /**< @brief Used by LoRa module */
+#define LoRa_MOSI SPI1_MOSI /**< @brief Used by LoRa module */
+#define LoRa_DIO0 17 /**< @brief Used by LoRa module */
+#define LoRa_RST 18  /**< @brief Used by LoRa module */
+
+#define SD_SCK SPI1_SCK /**< @brief SD clock pin */
 #define SD_CS 27 /**< @brief SD chip select pin */
-#define SD_MISO 12 /**< @brief SD miso pin */
-#define SD_MOSI 14 /**< @brief SD mosi pin */
+#define SD_MISO SPI1_MISO /**< @brief SD miso pin */
+#define SD_MOSI SPI1_MOSI /**< @brief SD mosi pin */
 
 #define GPS_RX 32 /**< @brief GPS rx pin */
 #define GPS_TX 33 /**<@brief GPS tx pin */
