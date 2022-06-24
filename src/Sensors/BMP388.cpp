01df310bda319eb6a375ae0068c7493924082383
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-06-24 19:04:27 +0200

    Written new average calculation. Need to test if not in train.

--- src/Sensors/BMP388.cpp
+++ src/Sensors/BMP388.cpp
@@ -9,6 +9,8 @@
 
 #include "Logging/SystemLogger.hpp"
 
+#define SLIDING_AVERAGE_ELEMENTS 10
+
 BMP388::BMP388()
         : Adafruit_BMP3XX(), GroundLevelPressure(0.0), counter_(0)
 {
@@ -26,7 +28,12 @@ bool BMP388::Calibrate()
     // Read latest data
     this->performReading();
 
-    if (counter_ == 0) {
+
+    double scalar1 = 1.0 / 100;
+    double scalar2 = 1.0 - scalar1;
+
+    if (counter_ == 0)
+    {
         GroundLevelPressure = this->pressure;
         counter_++;
         return false;
@@ -34,13 +41,38 @@ bool BMP388::Calibrate()
 
     // Calculates the average until two values are close enough together.
     //@todo Change to a better average calculation
+    /* Example: Sliding average of 10 elements
+
+    bool first_sample = true;
+    double average=0.0;
+    while(someCondition)
+    {
+       double newSample = getSample();
+       if(first_sample)
+       {
+        // everybody forgets the initial condition *sigh*
+          average = newSample;
+          first_sample = false;
+       }
+       else
+       {
+          average = (sample*scalar1) + (average*scalar2);
+       }
+     }
+
+     */
     GroundLevelPressure = (GroundLevelPressure + this->pressure) / 2;
 
+//    GroundLevelPressure = GroundLevelPressure * scalar2 + this->pressure * scalar1;
+
+
     // Check before and after difference
-    if (abs(this->pressure - GroundLevelPressure) < 0.01f) {
+    if (abs(this->pressure - GroundLevelPressure) < 0.01f)
+    {
         return true;
     }
 
-    slog_d("Ground level pressure: %.2f, difference: %f", GroundLevelPressure, abs(this->pressure - GroundLevelPressure));
+    slog_d("Pressure: %.2f, Ground level pressure: %.2f, difference: %f", this->pressure, GroundLevelPressure,
+           abs(this->pressure - GroundLevelPressure));
     return false;
 }
