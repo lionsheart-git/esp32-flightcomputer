a37db06ce0d7897d9fd51819c86f9bc69bbcbf20
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-10-09 13:27:25 +0200

    Added parser

--- src/main.cpp
+++ src/main.cpp
@@ -35,8 +35,72 @@ void setup()
     // Starting SD Card
     SDCard::Begin(sdSpi);
 
-    // Starting and calibrating Sensors
+    // Starting sensors
     Sensors.Begin();
+
+//    SPIClass loraSpi(HSPI);
+//    loraSpi.setClockDivider(32);
+//    loraSpi.begin(LoRa_SCK, LoRa_MISO, LoRa_MOSI, LoRa_CS);
+//    pinMode(sdSpi.pinSS(), OUTPUT);
+//    LoRaCommunication lora = LoRaCommunication(SPI);
+
+    // Starting flight control
+    FlightControl fc = FlightControl(Sensors);
+
+    bool readyForStart = false;
+    String input;
+    String substring;
+
+    while (!readyForStart)
+    {
+        if (Serial.available())
+        {
+            input = Serial.readStringUntil('\n');
+            Serial.printf("Gotten: %s\n", input.c_str());
+
+            int index = input.indexOf(' ');
+            substring = input.substring(0, index);
+
+            Serial.printf("Tokenized: %s\n", substring.c_str());
+
+            if (substring == "FC+START")
+            {
+                readyForStart = true;
+                break;
+            }
+            else if (substring == "SV+OPEN")
+            {
+                fc.OpenRecovery();
+            }
+            else if (substring == "SV+CLOSE")
+            {
+                fc.CloseRecovery();
+            }
+            else if (substring == "SV+MAX")
+            {
+                substring = input.substring(index + 1);
+                int degrees = substring.toInt();
+                fc.ServoMaxPosition(degrees);
+            }
+            else if (substring == "SV+MIN")
+            {
+                substring = input.substring(index + 1);
+                int degrees = substring.toInt();
+                fc.ServoMinPosition(degrees);
+            } else if (substring == "SYS+RESET") {
+                ESP.restart();
+            }
+            else {
+                Serial.println("Could not match input.");
+            }
+        }
+        else {
+            Serial.println("No input.");
+        }
+        delay(1000);
+    }
+
+    // Calibrate sensors
     Sensors.Calibrate();
 //
     SPIClass loraSpi(HSPI);
