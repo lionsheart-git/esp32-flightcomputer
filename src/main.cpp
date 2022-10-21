5a71fafbdf1800d9cefe94d89afaee193f10e74a
Patrick Gloeckner <p.gloeckner@tu-braunschweig.de>
2022-10-21 17:47:47 +0200

    Added printout of data during waiting for input

--- src/main.cpp
+++ src/main.cpp
@@ -100,6 +100,19 @@ void setup()
         }
         else {
             Serial.println("No input.");
+
+            Sensors.UpdateData();
+
+            printf("Time: %s\n", RTC.Timestamp());
+            printf("Sats: %i\n", Sensors.GNSS_Satellites());
+            printf("Temperature: %.2f *C\n", Sensors.Temperature());
+            printf("Pressure: %0.2f hPa, Filtered pressure: %0.2f hPa\n", Sensors.Pressure() / 100,
+                   Sensors.FilteredPressure() / 100);
+            printf("Acceleration X: %f, Y: %f, Z: %f m/s^2\n", Sensors.Acceleration().x, Sensors.Acceleration().y,
+                   Sensors.Acceleration().z);
+            printf("Rotation X: %f, Y: %f, Z: %f\n", Sensors.Gyro().x, Sensors.Gyro().y, Sensors.Gyro().z);
+            printf("Max Altitude: %.2f m\n", fc.MaxAltitude());
+            printf("Phase: %d\n", fc.Phase());
         }
         Sensors.SmartDelay(1000);
     }
